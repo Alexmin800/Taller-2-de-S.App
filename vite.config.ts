4d262287b16aeac158c1adc6f064924e5e3d135b
sankevin.es <aminchala.es@tecnologicoargos.edu.ec>
2025-06-05 15:55:32 -0500

    Version inicial

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "https://github.com/Alexmin800/Taller-2-de-S.A",
+  // build: {
+  //   outDir: "dist",
+  //   assetsDir: "assets",
+  //   sourcemap: true,
+  // },
+});
